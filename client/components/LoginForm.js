4d6704e5755f2115e53865fd607fbe54b96b3e95
Jose Francisco Peña Estrada <jose.francisco.pees@gmail.com>
2017-06-08 13:00:48 -0500

    finished login form, checks for errors in mutation

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
 import AuthForm from './AuthForm';
 import mutation from '../mutations/Login';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { errors: [] };
+    }
     onSubmit({ email, password }) {
         this.props.mutate({
-            variables: { email, password }
-        })
+            variables: { email, password },
+            refetchQueries: [{ query }]
+        }).catch(res => {
+            const errors = res.graphQLErrors.map(err => err.message);
+            this.setState({ errors });
+        });
     }
 
     render() {
         return (
             <div>
                 <h3>Login</h3>
-                <AuthForm onSubmit={this.onSubmit.bind(this)} />
+                <AuthForm
+                    onSubmit={this.onSubmit.bind(this)}
+                    errors={this.state.errors}
+                />
             </div>
         );
     }
 }
 
-export default graphql(mutation)(LoginForm);
+export default compose(
+    graphql(mutation),
+    graphql(query)
+)(LoginForm);
